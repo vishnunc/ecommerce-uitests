60b8b69e0e91e69ea4d2afc3c2a6c875f7ef0998
vishnunc <31437273+vishnunc@users.noreply.github.com>
2018-08-22 12:17:29 +0530

    Update Hooks.java
    Updated code to use chrome.exe

--- src/test/java/gradle/cucumber/Hooks.java
+++ src/test/java/gradle/cucumber/Hooks.java
@@ -29,7 +29,7 @@ public class Hooks {
 		try 
 		{
 			UIDriver.configprop.load(new FileInputStream("config/config.properties"));
-			
+			System.setProperty("webdriver.chrome.driver","drivers/chrome.exe");
 			if(UIDriver.configprop.getProperty("Browser").equalsIgnoreCase("firefox"))
 			{
 				UIDriver.driver = new FirefoxDriver();		
